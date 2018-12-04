b2946249d4ef447f7916525e95b9762eece4009f
adarshmathew <31207738+adarshmathew@users.noreply.github.com>
2018-12-04 22:25:02 +0530

    - Batch 3 & 4 of tweets downloaded - Edit gitignore to leave out auth tokens

--- code/1 - Tweet Extraction.R
+++ code/1 - Tweet Extraction.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(lubridate)
 library(rtweet)
 
-election_day.ids <- read_delim(paste(getwd(),"/data/dataverse/election-day.txt", sep = ""), 
+election_day.ids <- read_delim(paste(getwd(),"/data/dataverse-files/election-day.txt", sep = ""), 
                                delim = "\n", col_names = F) %>%
   rename(tweet.id = X1) %>%
   mutate(tweet.id = as.character(tweet.id))
@@ -14,22 +14,51 @@ tweet_sampler.details <- function(status_id.list, n_samples){
   tweets_samples.out <- status_id.list %>%
     sample_n(size = n_samples) %>%
     group_by(tweet.id) %>%
-    do(xx = lookup_statuses(.$tweet.id)) %>%
-    unnest()
+    do(xx = lookup_statuses(.$tweet.id))
   
   return(tweets_samples.out)
 }
+
+scraped.ids <- bind_rows(election_day.tweets_1 %>%
+                           select(tweet.id),
+                         election_day.tweets_2 %>%
+                           select(tweet.id),
+                         election_day.tweets_3.raw %>%
+                           select(tweet.id), 
+                         election_day.tweets_4.raw %>%
+                           select(tweet.id))
 
 # election_day.tweets_1 <- tweet_sampler.details(election_day.ids, 100000)
 # election_day.tweets_2 <- tweet_sampler.details(election_day.ids %>% anti_join(election_day.tweets_1 %>% 
 #                                                                                 select(tweet.id)),
 #                                                100000)
+# election_day.tweets_3.raw <- tweet_sampler.details(election_day.ids %>% 
+#                                                  anti_join(election_day.tweets_1 %>%
+#                                                              select(tweet.id)) %>%
+#                                                  anti_join(election_day.tweets_2 %>%
+#                                                              select(tweet.id)),
+#                                                100000)
 # 
+# election_day.tweets_3 <- election_day.tweets_3.raw %>% 
+#   unnest() %>%
+#   mutate(tweet.id = as.character(tweet.id)) %>%
+#   write_rds(paste(getwd(),"/data/election_day-tweets_3.rds", sep = ""))
+
+election_day.tweets_4.raw <- tweet_sampler.details(election_day.ids %>%
+                                                     anti_join(scraped.ids),
+                                                   250000)
+
+election_day.tweets_4 <- election_day.tweets_4.raw %>% 
+  unnest() %>%
+  mutate(tweet.id = as.character(tweet.id)) %>%
+  write_rds(paste(getwd(),"/data/election_day-tweets_4.rds", sep = ""))
+
+ 
 # write_rds(election_day.tweets_1, paste(getwd(),"/data/election_day-tweets_1.rds", sep = ""))
 # write_rds(election_day.tweets_2, paste(getwd(),"/data/election_day-tweets_2.rds", sep = ""))
 
 election_day.tweets_1 <- read_rds(paste(getwd(),"/data/election_day-tweets_1.rds", sep = ""))
-election_day.tweets_2 <- read_rds(paste(getwd(),"/data/election_day-tweets_2.rds", sep = ""))
+election_day.tweets_2 <- read_rds(paste(getwd(),"/data/election_day-tweets_2.rds", sep = "")) %>% mutate(tweet.id = as.character(tweet.id))
 
 election_day.tweets_comb <- bind_rows(election_day.tweets_1, 
                                       election_day.tweets_2 %>% 
